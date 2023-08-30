f069058cf68b9e7bec5eee59f5e1c8a3ad7e1bad
Filip Kober <koberfilip2@gmail.com>
2023-08-30 18:31:09 +0200

    forgot to add to commit

--- app.ts
+++ app.ts
@@ -4,15 +4,28 @@ import multer from '@koa/multer';
 import fileRoutes from './routes';
 import 'dotenv/config'
 import serve from 'koa-static';
+import views from '@ladjs/koa-views';
 
 const app = new koa();
 const router = new Router();
+
+const render = views(__dirname + '/views', {
+    map: {
+        html: 'ejs'
+    }
+})
+
+app.use(render);
 
 app.use(serve(process.env.MEDIA_PATH || './media'));
 
 router.get('/hello', (ctx) => {
     ctx.body = 'Hello World!';
 })
+
+router.get('/', async (ctx) => {
+    await ctx.render('index');
+});
 
 router.use(fileRoutes.routes(), fileRoutes.allowedMethods());
 
